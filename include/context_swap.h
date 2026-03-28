bb51442ac81e9d103f94c0eca99b3a5e2cffbae9
Wasmir Chowdhury <wasmirchowdhury@gmail.com>
2026-03-28 00:59:35 -0400

    first version context swap (baby steps)

---
+++ include/context_swap.h
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+struct Context {
+  void *rip, *rsp;
+  void *rbx, *rbp, *r12, *r13, *r14, *r15;
+};
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void get_context(Context *c);
+void set_context(Context *c);
+
+#ifdef __cplusplus
+}
+#endif
+
+// extern void context_swap(void);
+// extern void get_context(Context c);
+// extern void set_context(Context c);
